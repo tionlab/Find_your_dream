be01a4340b5e1103d3173ef20d874c807e4bbf67
TATION <96953236+tionlab@users.noreply.github.com>
2022-04-24 11:18:11 +0900

    Update wow.js

--- wow.js
+++ wow.js
@@ -1,9 +1,11 @@
 module.exports = {
-    name:"테스트",
+    name:"화이자",
     execute(message){
         const embeding = new (require("discord.js")).MessageEmbed()
             .setTitle("성공")
-            .addField("성공하셨습니다!")
+            .setDescription("성공하셨습니다!")
             .setTimestamp()
-            .setColor("GREEN")
-        message.reply(embeding)
+            .setColor("RANDOM")
+        message.reply({ embeds: [embeding] })
+    }
+}
